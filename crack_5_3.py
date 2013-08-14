55919191fb3fbab2ca494ca286f25fa7ec1357e8
xeostream <iostreamin@gmail.com>
2013-08-14 22:31:49 +0800

    question 5.3 edited

--- crack_5_3.py
+++ crack_5_3.py
@@ -23,7 +23,7 @@ def nextAndPreview(x):
 #smart method 
 def smart_nextAndPreview(x):
 	maxs = mins = x
-	max_move = min_move = 0
+	max_move = 0
 	while True:
 		if maxs & 1 == 0:
 			maxs >>= 1
@@ -37,31 +37,31 @@ def smart_nextAndPreview(x):
 			max_move += 1
 			maxs <<= max_move
 			break
-	print maxs
-	while True:
-		if mins & 1 == 1:
-			mins >>= 1
-			min_move += 1
-		elif mins & 2 == 0:
-			mins >>= 1
-			min_move += 1
-		elif mins & 2 == 2:
-			mins >>= 2
-			min_move += 2
-			mins <<= min_move
-			break
-		else:
-			mins = 0
-			break
 	for c in range(count_one(x)-count_one(maxs)):
 		temp = 1
 		temp <<= c
 		maxs |= temp
-	for c in range(count_one(x)-count_one(mins)):
-		temp = 1
-		temp <<= c
-		mins |= temp
-	print maxs, mins
+	bit = 0
+	while bit < 32 and (mins&1)!=0:
+		mins >>= 1
+		bit += 1
+	while bit < 32 and (mins&1)!=1:
+		mins >>= 1
+		bit += 1
+	mins <<= bit
+	if bit == 32: mins = 0
+	temp = count_one(x) - count_one(mins)
+	x >>= bit
+	print temp
+	while temp > 0:
+		mins = (mins<<1) | 1
+		temp -= 1
+		bit -= 1
+	print bit
+	while bit >0:
+		mins <<= 1
+		bit -= 1
+	print maxs, ~(mins)
 
 if __name__ == '__main__':
 	smart_nextAndPreview(1023)
